44b4e789b22d8dcb04c65101353ea5d82e5d26d5
Mikala Smith <mikalasmith@wifi_guest-10-38-116-59.uncc.edu>
2018-05-03 17:05:08 -0400

    sign up page validation

--- assets/java/java.js
+++ assets/java/java.js
@@ -1,92 +1,98 @@
-$.backstretch("http://res.cloudinary.com/codingbootcamp/image/upload/c_fit,h_700/v1524791753/background.jpg");
+// $.backstretch("http://res.cloudinary.com/codingbootcamp/image/upload/c_fit,h_700/v1524791753/background.jpg");
 
-  $(".container").hide();
-  $("#add-user").click(function(event) {
-    event.preventDefault();
-    $(".container").show();
-    $("#sign-up-form").toggle();
-    $("#add-user").toggle();
-    var config = {
-      apiKey: "AIzaSyCyq05OybcgW5FOvPiDrBQscEWl_p2BPiA",
-      authDomain: "recipe-app-3ba55.firebaseapp.com",
-      databaseURL: "https://recipe-app-3ba55.firebaseio.com",
-      projectId: "recipe-app-3ba55",
-      storageBucket: "",
-      messagingSenderId: "1053202008671"
-    };
+$(".container").hide();
 
-    firebase.initializeApp(config);
+$("#add-user").click(function (event) {
+  event.preventDefault();
+  // Initial Values
+  var name = "";
+  var email = "";
+  var zipcode = "";
+  name = $("#exampleInputName").val().trim();
+  email = $("#exampleInputEmail1").val().trim();
+  zipcode = $("#exampleInputZipcode").val().trim();
+  console.log(name);
+  console.log(email);
+  console.log(zipcode);
+  //empty out input fields after last user
+  $("#exampleInputName").val("");
+  $("#exampleInputEmail1").val("");
+  $("#exampleInputZipcode").val("");
 
-    // Create a variable to reference the database
-    var database = firebase.database();
+  if(!name==""&&!email== " "&&!zipcode==""){
+ $(".container").show();
+  $("#sign-up-form").toggle();
+  $("#add-user").toggle();
+  var config = {
+    apiKey: "AIzaSyCyq05OybcgW5FOvPiDrBQscEWl_p2BPiA",
+    authDomain: "recipe-app-3ba55.firebaseapp.com",
+    databaseURL: "https://recipe-app-3ba55.firebaseio.com",
+    projectId: "recipe-app-3ba55",
+    storageBucket: "",
+    messagingSenderId: "1053202008671"
+  };
 
-    // Initial Values
-    var name = "";
-    var email = "";
-    var zipcode = 0;
-    $("#add-user").on("click", function(event) {
-      // Don't refresh the page!
-      event.preventDefault();
+  firebase.initializeApp(config);
 
-      // YOUR TASK!!!
-      // Code in the logic for storing and retrieving the most recent user.
-      // Don't forget to provide initial data to your Firebase database.
-      name = $("#name-input").val().trim();
-      email = $("#email-input").val().trim();
-      age = $("#zipcode-input").val().trim();
-
-      database.ref().set({
-        name: name,
-        email: email,
-        zipcode: zipcode
-      });
-
-    });
+  // Create a variable to reference the database
+  var database = firebase.database();
+  database.ref().set({
+    name: name,
+    email: email,
+    zipcode: zipcode
   });
+  
+  }else{
+    $(".error-messages").text("The form is not complete. ").show();
+  }
+ 
+  
 
-  $("#recipe-search").on("click", function(event) {
-    event.preventDefault();
+});
 
-    // In this case, the "this" keyword refers to the button that was clicked
-    var ingredients = $("#ingredient-search").val();
-    console.log(ingredients);
+$("#recipe-search").on("click", function (event) {
+  event.preventDefault();
 
-    var recipequeryURL = "https://api.edamam.com/search?q=" +
-      ingredients + "&app_id=34b92ab5" + "&app_key=db5e2409c61531b092a4927d225ce0c8";
+  // In this case, the "this" keyword refers to the button that was clicked
+  var ingredients = $("#ingredient-search").val();
+  console.log(ingredients);
 
-    $.ajax({
-        url: recipequeryURL,
-        method: "GET"
-      })
-      .then(function(response) {
-        var results = response.hits;
+  var recipequeryURL = "https://api.edamam.com/search?q=" +
+    ingredients + "&app_id=34b92ab5" + "&app_key=db5e2409c61531b092a4927d225ce0c8";
 
-        var recipeList = $("<ul>");
+  $.ajax({
+    url: recipequeryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+      var results = response.hits;
 
-        for (var i = 0; i < results.length; i++) {
+      var recipeList = $("<ul>");
 
-          var list = $("<li>").text(results[i].recipe.label + " " + results[i].recipe.url);
-          recipeList.append(list);
-        }
-        $("#recipe-section").empty();
-        $("#recipe-section").append(recipeList);
-      });
-      var userZipcode = 28277;
-var yelpqueryURL = "https://api.yelp.com/v3/businesses/search?location=" + userZipcode + "&radius=8046&limit=10";
-    $.ajax({
-      url: yelpqueryURL,
-      method: "GET",
-      headers: {
-        "Authorization": "Bearer -yWLl5z5MtqpT8QUaKOFUYIGnGrgetYRnOV5DUM0yWGVgbs82xXIYXWf7H-TcDhHnRQXT15KUYOxj0gueIrFtLUgNc9_IqwJIRwuZi2Qqbcj0krqcLgyiGOh99zoWnYx"
-
-      }
-    })
-
-    .then(function(response) {
-      var results = response.businesses;
-      var yelpList= $("<ul>");
       for (var i = 0; i < results.length; i++) {
-        var list = $("<li>").text(results[i].name +" " + results[i].url);
+
+        var list = $("<li>").text(results[i].recipe.label + " " + results[i].recipe.url);
+        recipeList.append(list);
+      }
+      $("#recipe-section").empty();
+      $("#recipe-section").append(recipeList);
+    });
+  var userZipcode = 28277;
+  var yelpqueryURL = "https://api.yelp.com/v3/businesses/search?location=" + userZipcode + "&radius=8046&limit=10";
+  $.ajax({
+    url: yelpqueryURL,
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer -yWLl5z5MtqpT8QUaKOFUYIGnGrgetYRnOV5DUM0yWGVgbs82xXIYXWf7H-TcDhHnRQXT15KUYOxj0gueIrFtLUgNc9_IqwJIRwuZi2Qqbcj0krqcLgyiGOh99zoWnYx"
+
+    }
+  })
+
+    .then(function (response) {
+      var results = response.businesses;
+      var yelpList = $("<ul>");
+      for (var i = 0; i < results.length; i++) {
+        var list = $("<li>").text(results[i].name + " " + results[i].url);
         yelpList.append(list);
       }
       $("#restaurant-section").empty();
@@ -94,11 +100,13 @@ var yelpqueryURL = "https://api.yelp.com/v3/businesses/search?location=" + userZ
 
     })
 
-  });
+});
 
 
 
   //
+
+
 
 
 
